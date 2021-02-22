5a2fc6c4dfdac4cbf755b3b96e6a156405ba03dc
Carter Baxter <timothy.baxter@gsa.gov>
2021-02-22 13:21:49 -0600

    Fixing error in settings

--- nrm_django/nrm_site/settings.py
+++ nrm_django/nrm_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iyw)8%x%76@_%wb+dc60cq^sih*&phiupi^z7+my4b+ocl)8g9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
