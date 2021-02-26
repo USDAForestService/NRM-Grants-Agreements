5112f9793cc966945e7e991c6a09e6f5a156443c
Carter Baxter <32077682+tbaxter-18f@users.noreply.github.com>
2021-02-26 10:42:56 -0600

    Issue #69: Grants table, editable in admin (#84)
    * First commit of grants table
    
    * Second pass at Grants model
    
    * Grants table, editable in admin
    
    * Initial commit of authority and notes tables

---
+++ nrm_django/grants/admin.py
@@ -0,0 +1,95 @@
+from django.contrib import admin
+
+from .models import Grant, GrantAuthority, Note
+
+class GrantAdmin(admin.ModelAdmin):
+    list_display = ('proj_title', 'application_id', 'application_type', 'status', 'app_submit_date', 'app_received_date', 'proposed_start_date')
+    list_filter = ('status',)
+    readonly_fields = ['created_date', 'created_in_instance', 'modified_date', "status_date"]
+    search_fields = ['proj_title', 'proj_desc', 'application_id']
+    #list_editable = ['status',]
+    fieldsets = (
+        ('Project info', {
+            'fields': (
+                'proj_title', 'proj_status', 'proj_desc',
+                ('proj_received_dt', 'proj_execution_dt', 'proj_start_dt', 'proj_obligation_dt'),
+                ( 'proj_expiration_dt', 'proj_close_dt', 'proj_cancellation_dt')
+            )
+        }),
+        ('Project info (Advanced)', {
+            'classes': ('collapse',),
+            'fields': (('proj_rwu', 'proj_cfda_no', 'proj_science_cd', 'project_congressional_district')),
+        }),
+        ('Application', {
+            'fields': (
+                ('application_id', 'application_type', 'app_submission_type'),
+                ('app_submit_date', 'app_received_date', )),
+        }),
+        ('Grant', {
+            'fields': (
+                'proposed_start_date', 'proposed_end_date', 'locked_ind',
+                ('status', 'status_date' ),
+                ('created_by', 'created_date', 'created_in_instance'),
+                ('modified_by', 'modified_date', 'modified_in_instance'),
+            ),
+        }),
+        ('Fed ID', {
+            'classes': ('collapse',),
+            'fields': (
+                ('fed_id_fy', 'fed_id_type', 'fed_id_agency', 'fed_id_region'),
+                ('fed_id_unit','fed_id_subunit', 'fed_id_seq' )
+            ),
+        }),
+        ('Other', {
+            'fields': (
+                'hhs_payment_ind',
+                'date_mailed',
+                'date_signed',
+                'extramural_ind',
+                'research_type',
+                'journal_ind',
+                'mod_number',
+                'orig_fed_id',
+                'comments',
+                'master_fed_id',
+                'aop_ind',
+                'geo_type',
+                'managing_state_county',
+                'areas_effected',
+                'ffin',
+                'state_identifier',
+                'state_eo_code',
+                'state_eo_date',
+                'fed_est_fund',
+                'applicant_est_fund',
+                'state_est_fund',
+                'local_est_fund',
+                'pi_est_fund',
+                'oth_est_fund',
+                'reroute_from',
+                'reroute_date',
+                'certificaion_date',
+                'ffis_doc_id',
+                'applicant_name',
+                'international_act_ind',
+                'proj_type',
+                'advance_allowed_ind',
+                'authority_approval',
+                'authority',
+                'format',
+                'other_approval',
+                'master_agreement_ind',
+                'progrm_responsibility_type',
+                'wppp_status',
+                'wppp_status_date',
+                'cooperator_agreement_number',
+                'admin_open',
+                'last_update'
+            )
+        })
+    )
+
+
+admin.site.register(Grant, GrantAdmin)
+admin.site.register(GrantAuthority)
+admin.site.register(Note)
