8312e0f3d8acbc6c103964a80669fcae5b7729c5
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2021-05-17 12:10:31 -0500

    configuring django settings

--- nrm_django/nrm_site/settings/eb.py
+++ nrm_django/nrm_site/settings/eb.py
@@ -5,13 +5,12 @@ from .base import *  # noqa
 def database_dict_from_rds_env():
     """Django database dict from Elastic Beanstalk's RDS env variables."""
     return {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 
 
