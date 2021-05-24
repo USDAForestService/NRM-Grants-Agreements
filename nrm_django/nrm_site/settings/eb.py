b0ad3cff839537a07ac6e4033144e870c4de277e
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2021-05-24 10:51:14 -0500

    Fix linting errors

--- nrm_django/nrm_site/settings/eb.py
+++ nrm_django/nrm_site/settings/eb.py
@@ -17,9 +17,12 @@ def database_dict_from_rds_env():
 
 
 DATABASES = {"default": database_dict_from_rds_env()}
-ALLOWED_HOSTS = ["development.eba-ny2imkrt.us-east-2.elasticbeanstalk.com","ga-development.eba-c5v3ebxp.us-gov-west-1.elasticbeanstalk.com", ]
+ALLOWED_HOSTS = [
+    "development.eba-ny2imkrt.us-east-2.elasticbeanstalk.com",
+    "ga-development.eba-c5v3ebxp.us-gov-west-1.elasticbeanstalk.com",
+]
 try:
-    EC2_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4').text
+    EC2_IP = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").text
     ALLOWED_HOSTS.append(EC2_IP)
 except requests.exceptions.RequestException:
     pass
