de5db3d5d225fff51218a70324568d7de96c4019
Neil MartinsenBurrell <neil.martinsen-burrell@gsa.gov>
2021-05-27 11:01:35 -0500

    Prepare to run on Elastic Beanstalk (#245)
    * config for EB with django
    
    * Trying to hook up the database
    
    * Configure settings for Elastic Beanstalk??
    
    * configuring django settings
    
    * Use correct port number for
    
    * Backup/restore documentation
    
    * cleanup for PR
    
    * Commit elasticbeanstalk config for CI purposes
    
    * modify eb config for initial testing
    
    * remove environment option for eb
    
    * no branch defaults
    
    * add elb name to django allowed hosts
    
    * use correct dns for allowed hosts
    
    * programatically add ec2 ip address to allowed hosts
    
    * add requests dependency
    
    * test without eb config file
    
    * Fix linting errors
    
    Co-authored-by: luther007 <37386113+LutherJolliff@users.noreply.github.com>

---
+++ nrm_django/nrm_site/settings/eb.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+from .base import *  # noqa
+
+
+def database_dict_from_rds_env():
+    """Django database dict from Elastic Beanstalk's RDS env variables."""
+    return {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["RDS_DB_NAME"],
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
+    }
+
+
+DATABASES = {"default": database_dict_from_rds_env()}
+ALLOWED_HOSTS = [
+    "development.eba-ny2imkrt.us-east-2.elasticbeanstalk.com",
+    "ga-development.eba-c5v3ebxp.us-gov-west-1.elasticbeanstalk.com",
+]
+try:
+    EC2_IP = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").text
+    ALLOWED_HOSTS.append(EC2_IP)
+except requests.exceptions.RequestException:
+    pass
--- nrm_django/nrm_site/settings/prod.py
+++ nrm_django/nrm_site/settings/prod.py
@@ -7,6 +7,7 @@ from .base import *  # noqa
 # Cloud.gov DB connection
 # See: https://pypi.org/project/dj-database-url/
 database_url = os.getenv("DATABASE_URL")
+if database_url:
+    DATABASES = {"default": dj_database_url.parse(database_url)}
 
-DATABASES = {"default": dj_database_url.parse(database_url)}
 ALLOWED_HOSTS = ["fs-nrm.app.cloud.gov"]
