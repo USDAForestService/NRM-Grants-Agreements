fd3c572e3360b77bc044052532f72f6de4bd8656
Carter Baxter <timothy.baxter@gsa.gov>
2021-06-10 08:00:41 -0500

    Updated to de-django-fy and uswds-ify instead

--- nrm_django/nrm_site/settings/prod.py
+++ nrm_django/nrm_site/settings/prod.py
@@ -10,4 +10,4 @@ database_url = os.getenv("DATABASE_URL")
 if database_url:
     DATABASES = {"default": dj_database_url.parse(database_url)}
 
-ALLOWED_HOSTS = ["fs-nrm.app.cloud.gov"]
+ALLOWED_HOSTS = ["fs-nrm.app.cloud.gov", "127.0.0.1"]
