3cfade11a6f8771fa883ced6132ae54f6cc5c573
Matthew J Cloyd <matt.cloyd@gsa.gov>
2021-08-15 10:15:46 -0400

    Add 20 or so more attributes, fix environment config so it's buildable/deployable to sandbox.

--- frontend/src/app/grant.service.spec.ts
+++ frontend/src/app/grant.service.spec.ts
@@ -9,6 +9,13 @@ describe('GrantService', () => {
     service = TestBed.inject(GrantService);
   });
 
+  /**
+   * @todo Set up a test to see what happens if there's type
+   * mismatches in the data for #getGrants. For example,
+   * if an applicationType value doesn't fall in the type range,
+   * what happens (null, runtime error, etc.)?
+   */
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
