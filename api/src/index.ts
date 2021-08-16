e1930a99a8a5d5957e232cb93fa8d4d20c9a470a
Matthew J Cloyd <matt.cloyd@gsa.gov>
2021-08-16 13:18:09 -0400

    Add USWDS

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
 
-const app  = express();
+const app = express();
 const port = 3000;
 
 /**
  * @todo Set using environment variables for security hardening.
  */
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin',  '*')
-  res.setHeader('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
@@ -20,11 +20,11 @@ enum Status {
 }
 
 enum AppSubmissionType {
-  New = 'NEW',
-  Application = 'Application',
-  Preapplication = 'Preapplication',
-  NonConstructionApplication = 'NON-CONSTRUCTION APPLICATION',
-  NonConstructionPreApplication = 'NON-CONSTRUCTION PRE-APPLICATION',
+  New = "NEW",
+  Application = "Application",
+  Preapplication = "Preapplication",
+  NonConstructionApplication = "NON-CONSTRUCTION APPLICATION",
+  NonConstructionPreApplication = "NON-CONSTRUCTION PRE-APPLICATION",
 }
 
 interface Grant {
@@ -41,35 +41,35 @@ const getGrants = (
 ) => {
   let grants: Grant[] = [
     {
-      cn: '100X100X100X',
-      projTitle: 'Forest Service Project',
+      cn: "100X100X100X",
+      projTitle: "Forest Service Project",
       projStatus: Status.None,
-      appSubmissionType: AppSubmissionType.New
+      appSubmissionType: AppSubmissionType.New,
     },
     {
-      cn: '101AB101AB101AB',
-      projTitle: 'Timber Project',
+      cn: "101AB101AB101AB",
+      projTitle: "Timber Project",
       projStatus: Status.One,
-      appSubmissionType: AppSubmissionType.Application
+      appSubmissionType: AppSubmissionType.Application,
     },
     {
-      cn: '202C202C202C',
-      projTitle: 'Swales & Marshes',
+      cn: "202C202C202C",
+      projTitle: "Swales & Marshes",
       projStatus: Status.One,
-      appSubmissionType: AppSubmissionType.Preapplication
+      appSubmissionType: AppSubmissionType.Preapplication,
     },
     {
-      cn: '020D020D020D',
-      projTitle: 'Pasture Land',
+      cn: "020D020D020D",
+      projTitle: "Pasture Land",
       projStatus: Status.Two,
-      appSubmissionType: AppSubmissionType.NonConstructionApplication
-    }
+      appSubmissionType: AppSubmissionType.NonConstructionApplication,
+    },
   ];
 
   response.status(200).json(grants);
-}
+};
 
-app.get('/grants', getGrants);
+app.get("/grants", getGrants);
 
 /**
  * @environment development
@@ -77,7 +77,7 @@ app.get('/grants', getGrants);
  * doesn't run app.listen.
  */
 app.listen(port, () => {
-  console.log(`NRM G&A API running on port ${port}.`)
+  console.log(`NRM G&A API running on port ${port}.`);
 });
 
 module.exports = app;
